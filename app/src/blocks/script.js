50a62fb4c559b7feba275cfee20cdbe0782e9b38
Olga <notist@mail.ru>
2016-12-05 18:00:27 +0300

    moving side-menu
    added animation of side menu
    changed footer on xs

--- app/src/blocks/script.js
+++ app/src/blocks/script.js
@@ -1,3 +1,13 @@
 /**
  * Created by Vasiliy Tsvetkov on 30.11.2016.
  */
+$(document).ready( function(){
+
+
+    $('.bars').click(function(){
+        $('.side-menu').addClass('opened');
+    });
+    $('.close').click(function () {
+        $('.side-menu').removeClass('opened');
+    })
+});
