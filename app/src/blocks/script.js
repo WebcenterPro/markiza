c1f5c5a2825004b487c60dc308ef1b93255bdb2c
Olga <notist@mail.ru>
2016-12-06 18:01:48 +0300

    slider
    improved side-menu and footer, added slider and section-header block

--- app/src/blocks/script.js
+++ app/src/blocks/script.js
@@ -1,13 +1,71 @@
 /**
  * Created by Vasiliy Tsvetkov on 30.11.2016.
  */
-$(document).ready( function(){
+$(document).ready(function () {
+    var window_width = $(window).width();
+    //  var menu_items = $('.navigation.top .navigation__item').children();
+    var catalog_items = $('.side-menu-items__item ').children();
 
-
-    $('.bars').click(function(){
+    $('.bars').click(function () {
         $('.side-menu').addClass('opened');
     });
     $('.close').click(function () {
         $('.side-menu').removeClass('opened');
     })
+
+
+    $('.side-menu-items__full > .side-menu-items__item:first-child').click(function() {
+        $(this).toggleClass('down');
+        $(this).next('.side-menu-items__catalog').slideToggle("slow");
+
+    });
+
+
+    $(window).resize(function () {
+        var window_width = $(window).width();
+
+        if (window_width > 1070) {
+            $('.side-menu').removeClass('opened');
+        }
+
+        if (window_width < 767) {
+
+            $('.side-menu-items__full > .side-menu-items__item:first-child')
+                .addClass("menu-arrow")
+                .after($(".side-menu-items__catalog"));
+
+        } else {
+            $('.side-menu-items').append($(".side-menu-items__catalog"));
+        }
+
+    });
+
+
+    if (window_width < 767) {
+
+        $('.side-menu-items__full > .side-menu-items__item:first-child')
+            .addClass("menu-arrow")
+            .after($(".side-menu-items__catalog"));
+
+    }
+
+
+    /****************************/
+    /*****OWL SLIDER INIT********/
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav:true,
+        navText : ["",""],
+        items: 1
+        // responsive: {
+        //     0: {
+        //         items: 1
+        //     }
+        // }
+
+    });
+
+    /****************************/
+
 });
--- app/src/libs.js
+++ app/src/libs.js
@@ -1,2 +1,4 @@
 @@include('..\libs\jquery\dist\jquery.js')
+@@include('..\libs\tether\dist\js\tether.js')
 @@include('..\libs\bootstrap\dist\js\bootstrap.js')
+@@include('..\libs\owl.carousel\dist\owl.carousel.js')
