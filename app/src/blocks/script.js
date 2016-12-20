f84a1db06fac4e131860b0df118f837774d69b93
Olga <notist@mail.ru>
2016-12-20 16:23:55 +0300

    Refactor
    And start item-card

--- app/src/blocks/script.js
+++ app/src/blocks/script.js
@@ -1,23 +1,16 @@
-$(document).ready(function() {
-
-//    var solution_block__nav_width = $(".solution-block__nav").width()+23;
-
+$(document).ready(function () {
 
     /*****OWL SLIDER INIT********/
 
-    $('.slider .owl-carousel').owlCarousel({
+    $('.main-slider').owlCarousel({
         loop: true,
         nav: true,
+        dots: false,
         autoplay: true,
         navText: ["", ""],
         items: 1
-        // responsive: {
-        //     0: {
-        //         items: 1
-        //     }
-        // }
-
     });
+
 
     $('.owl-carousel.client-slider').owlCarousel({
         loop: true,
@@ -27,21 +20,81 @@ $(document).ready(function() {
         autoplayHoverPause: true,
         // margin:30,
         navText: ["", ""],
-        responsive:{
-            0:{
-                items:2
+        responsive: {
+            0: {
+                items: 2
             },
-            600:{
-                items:3
+            600: {
+                items: 4
             },
-            1000:{
-                items:6
+            850: {
+                items: 6
+            },
+            1070: {
+                items: 5
+            },
+            1350: {
+                items: 6
+            },
+            1500: {
+                items: 7
             }
         }
 
     });
 
-    //solutions block
+//init slider for item-card  with thumbnails
+
+
+    $('.itemcard-slider').owlCarousel({
+        loop: true,
+        nav: true,
+        dots: true,
+        autoplay: true,
+        navText: ["", ""],
+        items: 1
+    });
+
+    // 1) ASSIGN EACH 'DOT' A NUMBER
+    dotcount = 1;
+    var owl_dot = $('.itemcard-slider .owl-dot');
+    var owl_item = $('.itemcard-slider .owl-item');
+
+    owl_dot.each(function () {
+        $(this).addClass('dotnumber' + dotcount);
+        $(this).attr('data-info', dotcount);
+        dotcount = dotcount + 1;
+    });
+
+    // 2) ASSIGN EACH 'SLIDE' A NUMBER
+    slidecount = 1;
+
+    owl_item.not('.cloned').each(function () {
+        $(this).addClass('slidenumber' + slidecount);
+        slidecount = slidecount + 1;
+    });
+
+    // SYNC THE SLIDE NUMBER IMG TO ITS DOT COUNTERPART (E.G SLIDE 1 IMG TO DOT 1 BACKGROUND-IMAGE)
+   owl_dot.each(function () {
+
+        grab = $(this).data('info');
+
+        slidegrab = jQuery('.slidenumber' + grab + ' img').attr('src');
+        console.log(slidegrab);
+
+       $(this).css("background-image", "url(" + slidegrab + ")");
+
+    });
+
+    // THIS FINAL BIT CAN BE REMOVED AND OVERRIDEN WITH YOUR OWN CSS OR FUNCTION, I JUST HAVE IT
+    // TO MAKE IT ALL NEAT
+    amount = owl_dot.length;
+    gotowidth = 98 / amount;
+
+    owl_dot.css("width", gotowidth + "%");
+    newwidth = owl_dot.width()/1.5;
+
+    owl_dot.height(newwidth);
 
 
 });
